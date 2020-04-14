8efb96f792cbe7ea873b6279a96cab51cce28ccf
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-04-14 16:30:16 -0300

    uploading-exercises

--- scripts/script.js
+++ scripts/script.js
@@ -4,18 +4,20 @@ hideProyectos();
 function hideEjercicios() {
     var section = document.querySelector(".ejercicios");
     var othersection = document.querySelector(".proyectos");
+    var btnSection = document.querySelector(".btn-ejercicios");
     if (section.style.display == "none") {
         section.style.display = "block";
-        section.classList.toggle("active");
+        btnSection.classList.toggle("active");
         othersection.style.display = "none";
     } else section.style.display = "none";
 }
 function hideProyectos() {
     var section = document.querySelector(".proyectos");
     var othersection = document.querySelector(".ejercicios");
+    var btnSection = document.querySelector(".btn-proyectos");
     if (section.style.display == "none") {
         section.style.display = "block";
-        section.classList.toggle("active");
+        btnSection.classList.toggle("active");
         othersection.style.display = "none";
     } else section.style.display = "none";
 }
