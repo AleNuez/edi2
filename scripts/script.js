a36fc767c3b2f1757c177accaf46b9d1a39012ac
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-04-19 01:23:35 -0300

    second-class

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,26 @@
 var divejercicios = document.querySelector(".ejercicios");
+var divejerciciosdos = document.querySelector(".ejerciciosdos");
 var divproyectos = document.querySelector(".proyectos");
 var btnejercicios = document.querySelector(".btn-ejercicios");
+var btnejerciciosdos = document.querySelector(".btn-ejerciciosdos");
 var btnproyectos = document.querySelector(".btn-proyectos");
+
 
 divejercicios.style.display = "block";
 divproyectos.style.display = "none";
 
-function hideEjercicios(divejercicios,divproyectos) {
+function hideEjercicios(divejercicios,divproyectos,divejerciciosdos) {
     divejercicios.style.display = "block";
     divproyectos.style.display = "none";
+    divejerciciosdos.style.display = "none";
 }
-function hideProyectos(divproyectos,divejercicios) {
+function hideEjerciciosdos(divejercicios,divproyectos,divejerciciosdos) {
+    divejerciciosdos.style.display = "block";
+    divproyectos.style.display = "none";
+    divejercicios.style.display ="none";
+}
+function hideProyectos(divproyectos,divejercicios,divejerciciosdos) {
     divproyectos.style.display = "block";
-    divejercicios.style.display = "none"; 
+    divejercicios.style.display = "none";
+    divejerciciosdos.style.display = "none"; 
 }
