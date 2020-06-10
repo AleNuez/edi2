c742554ffd5ffb3c6f11e0d4cff7bd918f0b1865
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-06-10 01:15:37 -0300

    forms-starting

--- scripts/proyecto-script.js
+++ scripts/proyecto-script.js
@@ -1,7 +1,18 @@
-
+console.log("ok");
 function hideNav() {
     var nav = document.querySelector("#navbar");
     if (nav.style.display == "none") {
         nav.style.display = "block";
     } else nav.style.display = "none";
 }
+
+var modalBtn = document.querySelector('.modal-login');
+var modalBg = document.querySelector('.modal-bg-login');
+var modalClose = document.querySelector('.modal-close');
+modalBtn.addEventListener('click',function(){
+    modalBg.classList.add('bg-active');
+});
+modalClose.addEventListener('click',function(){
+    modalBg.classList.remove('bg-active');
+});
+
