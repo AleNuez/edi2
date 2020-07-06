3e19c3ffee7131fd127379ac8d15b6f0bde15622
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-07-06 03:14:43 -0300

    savingfix

--- docs/proyecto/save-pic.php
+++ docs/proyecto/save-pic.php
@@ -1,10 +1,42 @@
 <?php
 
+
+
+function resize_image($file, $w, $h, $crop=FALSE) {
+    list($width, $height) = getimagesize($file);
+    $r = $width / $height;
+    if ($crop) {
+        if ($width > $height) {
+            $width = ceil($width-($width*abs($r-$w/$h)));
+        } else {
+            $height = ceil($height-($height*abs($r-$w/$h)));
+        }
+        $newwidth = $w;
+        $newheight = $h;
+    } else {
+        if ($w/$h > $r) {
+            $newwidth = $h*$r;
+            $newheight = $h;
+        } else {
+            $newheight = $w/$r;
+            $newwidth = $w;
+        }
+    }
+    $src = imagecreatefromjpeg($file);
+    $dst = imagecreatetruecolor($newwidth, $newheight);
+    imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+    return $dst;
+}
+
 include('conexion.php');
 session_start();
 $user = $_SESSION['username'];
-$Img = addslashes(file_get_contents($_FILES['Imagen']['tmp_name'])); //tmp_name: nombre del arhivo en el SO
+$imagen = $_FILES['Imagen']['tmp_name'];
 
+
+$Img = addslashes(file_get_contents($imagen)); //tmp_name: nombre del arhivo en el SO
+//$Img = resize_image($imagen, 200, 200);
 $saveImg = "UPDATE users SET image='$Img' WHERE user = '$user'";
 
 $resultadosave = $conexion->query($saveImg);
