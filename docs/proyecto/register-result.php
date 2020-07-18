306cc7507de3907ac752a4cdddc93928bdd732a5
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-07-18 15:29:42 -0300

    registered-resalt

--- docs/proyecto/register-result.php
+++ docs/proyecto/register-result.php
@@ -1,7 +1,6 @@
 <?php
      session_start();
    $rol = $_SESSION['rol'];
-  
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,25 +8,23 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../../styles/proyecto-styles.css">
-
         <title>Login - ConcursAR</title>
-      
-
     </head>
     <body>
-      
         <header id="header">
+        <div class="header-concursar">
             <img src="../../img/escudo.png" class="escudo-header">
             <h2 class="header-brand">
                 ConcursAR</h2>
+                </div>
         </header>
         <main>
             <div class="main-without-nav">
                 <article id="article">
                     <div class="hero-1">
                         <div class="white-font">
-                            <h1 class="custom-h1">Registrado Correctamente!</h1>
-                            
+                            <h1 class="custom-h1 registered-h1">Registrado Correctamente!</h1>
+                            <div class="main-begin-btn-registered">
                             <?php 
                             switch($rol){
     case "profesor":
@@ -60,17 +57,18 @@
             </main>
             <footer id="footer">
 
-                <p class="copyright">
-                    <h2>
-                        <small>Mas información</small>
-                    </h2>
-                    <a href="https://www.argentina.gob.ar/educacion">Ministerio de educación de la Nación</a>
-                    |
-                    <a href="https://www.argentina.gob.ar">Portal de la Nación Argentina</a>
-                    |
-                    <a href="http://www.abc.gov.ar">Portal de educación de la Prov de Buenos Aires</a>
-                </p>
-            </footer>
+<h2 class="footer-more-info">
+    <small>Mas información</small>
+</h2>
+<div class="footer-links">
+    <a class="footer-a-tags" href="https://www.argentina.gob.ar/educacion">| Ministerio de educación de la Nación |</a>
+
+    <a class="footer-a-tags" href="https://www.argentina.gob.ar">| Portal de la Nación Argentina |</a>
+
+    <a class="footer-a-tags" href="http://www.abc.gov.ar">| Portal de educación de la Prov de Buenos Aires |</a>
+</div>
+
+</footer>
             <script src="/scripts/proyecto-script.js"></script>
         </body>
     </html>
