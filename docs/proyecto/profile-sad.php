afcabe5f0dace466ea3636307c3613d53ee90616
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-08-02 20:18:54 -0300

    sad

--- docs/proyecto/profile-sad.php
+++ docs/proyecto/profile-sad.php
@@ -7,6 +7,7 @@ $convertedName = ucfirst($name);
 $surname = $_SESSION['surname'];
 $convertedSurname = ucfirst($surname);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -18,8 +19,9 @@ $convertedSurname = ucfirst($surname);
     </head>
     <body>
 
-        <header id="header">
-            <div class="header-escudo">
+<header id="header">
+
+            <div class="header-concursar">
                 <a href="./home-sad.php" class="header-link"><img src="../../img/escudo.png" class="escudo-header">
                     <h2 class="header-brand">
                         ConcursAR</h2>
@@ -50,13 +52,14 @@ $convertedSurname = ucfirst($surname);
         </header>
         <main>
 
+
             <div class="main-without-nav">
                 <article id="article">
-                    <div class="hero-5 ">
+                    <div class="hero hero-5 profile-padding">
 
                         <div class="white-font flex-parent">
                             <div class="col-1">
-                                <h1 class="custom-h1 smaller">Perfil Administrativo (SAD)</h1>
+                                <h1 class="custom-h1 smaller profile-title">Perfil Administrativo (SAD)</h1>
                                 <div class="profile-photo">
                                     <?php
                 $buscarImgs = "SELECT image FROM users WHERE user='$usuario' ";
@@ -77,6 +80,7 @@ $convertedSurname = ucfirst($surname);
                                     <button id="profile-pic-btn" class="prof-btn white-font">Cambiar foto de perfil</button>
                                 </div>
                             </div>
+
                             <div class="col-2">
                                 <h2 class="custom-text welcome-text">
                                     ¿Que desea hacer?
@@ -98,7 +102,7 @@ $convertedSurname = ucfirst($surname);
                     </div>
 
                 </article>
-
+            <!-- empiza modal profile picture -->
                 <div class="tabla">
                     <div class="profile-pic-modal">
                         <form
@@ -123,21 +127,23 @@ $convertedSurname = ucfirst($surname);
 
                             <div style="justify-content:space-between; display:flex;" class="botonera">
                                 <input
-                                    style="padding:0rem 3rem;margin:0.3rem 0.3rem;"
+                                   
                                     type="submit"
                                     class="login-btn"
                                     value="Cargar">
-                                <a class="close-btn" href="./profile-sad.php">Cancelar</a>
+                                <!-- <a class="close-btn" href="./profile-sad.php">Cancelar</a> -->
                             </div>
                         </form>
                     </div>
-                    <!-- termina modal profile -->
+                    <!-- termina modal profile piture -->
+
+                    <!-- empieza modal view profile -->
 
                     <div class="profile-view-modal">
                         <form
-                            class="profile-view-modal-style"
+                            class="profile-view-modal-style view-profile-spec"
                             method="POST"
-                            action="./profile-doc.html">
+                            action="./profile-sad.html">
 
                             <span class="profile-view-close">X</span>
                             <div class="flex-parent">
@@ -152,11 +158,11 @@ $convertedSurname = ucfirst($surname);
             ?>
                                         <img
                                             src="data:image/jpg;base64,<?php echo base64_encode($colocar['image']); ?>"
-                                            class="user-profile">
+                                            class="user-profile smaller-profile">
                                     <?php
             }else {
             ?>
-                                        <img src="../../img/default-user.jpg" class="user-profile">
+                                        <img src="../../img/default-user.jpg" class="user-profile smaller-profile">
                                         <?php
             }
             ?>
@@ -206,20 +212,22 @@ $convertedSurname = ucfirst($surname);
                                 <a href="./profile-sad.php" class="close-btn">Cerrar</a>
                             </form>
                         </div>
-                        <!-- termina modal profile -->
+
+                        <!-- termina modal profile view -->
                         <div class="profile-edit-modal">
                             <!-- FORM EDIT -->
                             <form action="./edit.php" method="POST" class="profile-edit-modal-style">
-                                <div class="reg-camp">
+                            <div class="all-forms">  
+                            <div class="reg-camp">
                                     <h3>Actualizar Datos</h3>
                                 </div>
                                 <div class="reg-camp">
-                                    <label for="login-user">DNI:</label>
+                                    <!-- <label for="login-user">DNI:</label>
                                     <input
                                         value="<?php echo "$show_dni"; ?>"
                                         type="number"
                                         name="edit-user"
-                                        required="REQUIRED">
+                                        required="REQUIRED"> -->
                                     <label for="reg-dob">Fecha de nacimiento:</label>
                                     <input
                                         type="date"
@@ -242,7 +250,7 @@ $convertedSurname = ucfirst($surname);
                                         name="edit-apellido"
                                         required="REQUIRED"></div>
                                 <div class="reg-camp">
-                                    <label for="reg-rol">Rol en la Plataforma:</label>
+                                    <!-- <label for="reg-rol">Rol en la Plataforma:</label>
                                     <select name="edit-rol" id="reg-rol" required="REQUIRED">
                                         <option value="NUL">-- Seleccione --</option>
                                         <optgroup label="Secretaria Asuntos Doc">
@@ -262,7 +270,7 @@ $convertedSurname = ucfirst($surname);
                                                 value="preceptor"
                                                 <?php if($show_cargo=="Preceptor"){echo "selected";} ?>>Preceptor</option>
                                         </optgroup>
-                                    </select>
+                                    </select> -->
                                     <label for="reg-area">Area:</label>
                                     <input value="<?php echo "$show_area"; ?>" type="text" name="edit-area">
                                     <label for="reg-especialidad">Especialidad:</label>
@@ -283,13 +291,13 @@ $convertedSurname = ucfirst($surname);
                                         value="<?php echo "$show_region"; ?>"
                                         type="text"
                                         name="edit-region"
-                                        required="REQUIRED">
+                                        required>
                                     <label for="reg-distrito">Distrito:</label>
                                     <input
                                         value="<?php echo "$show_distrito"; ?>"
                                         type="text"
                                         name="edit-distrito"
-                                        required="REQUIRED">
+                                        required>
                                 </div>
                                 <div class="reg-camp">
                                     <label for="reg-documentacion">Documentación:</label>
@@ -298,24 +306,27 @@ $convertedSurname = ucfirst($surname);
                                         type="file"
                                         name="edit-documentacion">
                                     <label for="reg-puntaje">Puntaje:</label>
-                                    <input value="<?php echo "$show_puntaje"; ?>" type="text" name="edit-puntaje">
+                                    <input class="puntaje-camp" value="<?php echo "$show_puntaje"; ?>" type="text" name="edit-puntaje">
                                 </div>
                                 <div class="reg-camp">
                                     <label for="reg-pass">Contraseña:</label>
-                                    <input
+                                    <div class="passandimg"><input
+                                        required
                                         id="hidden-password"
-                                        value="<?php echo "$show_password"; ?>"
+                                        class="pass-camp"
                                         type="password"
                                         name="edit-pass">
                                     <img onmousedown="showpass()" onmouseup="hidepass()" class="view-password"></img>
+                                    </div>
                                     <label for="reg-pass-repeat">
                                         Confirme Contraseña:</label>
                                     <input
-                                        value="<?php echo "$show_password"; ?>"
+                                        
                                         type="password"
                                         name="edit-pass-repeat"></div>
                                 <input type="submit" class="login-btn" value="Actualizar">
                                 <span class="profile-edit-close">X</span>
+                                </div> <!-- termina all forms -->
                             </form>
                         </div>
                         <!-- termina modal profile -->
@@ -323,17 +334,18 @@ $convertedSurname = ucfirst($surname);
                 </main>
                 <footer id="footer">
 
-                    <p class="copyright">
-                        <h2>
-                            <small>Mas información</small>
-                        </h2>
-                        <a href="https://www.argentina.gob.ar/educacion">Ministerio de educación de la Nación</a>
-                        |
-                        <a href="https://www.argentina.gob.ar">Portal de la Nación Argentina</a>
-                        |
-                        <a href="http://www.abc.gov.ar">Portal de educación de la Prov de Buenos Aires</a>
-                    </p>
-                </footer>
+<h2 class="footer-more-info">
+    <small>Mas información</small>
+</h2>
+<div class="footer-links">
+    <a class="footer-a-tags" href="https://www.argentina.gob.ar/educacion">| Ministerio de educación de la Nación |</a>
+
+    <a class="footer-a-tags" href="https://www.argentina.gob.ar">| Portal de la Nación Argentina |</a>
+
+    <a class="footer-a-tags" href="http://www.abc.gov.ar">| Portal de educación de la Prov de Buenos Aires |</a>
+</div>
+
+</footer>
                 <script src="../../scripts/profile-script.js"></script>
                 <script>
                     function showpass() {
