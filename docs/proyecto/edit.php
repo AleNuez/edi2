d0d39692c21f0a5176c3a103f8f8e7b6d99ed304
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-08-02 02:33:38 -0300

    edit-done

--- docs/proyecto/edit.php
+++ docs/proyecto/edit.php
@@ -6,11 +6,11 @@ session_start();
 $globaluser = $_SESSION['username'];
 $globalpassword = $_SESSION['globalpassword'];
 
-$usuario = $_POST['edit-user']; //usuario y clave enviado por usuario en el form
+// $usuario = $_POST['edit-user']; //usuario y clave enviado por usuario en el form
 $nombre = $_POST['edit-nombre'];
 $apellido = $_POST['edit-apellido'];
 $dob = $_POST['edit-dob'];
-$cargo = $_POST['edit-rol'];
+$cargo = $_SESSION['rol'];
 $area = $_POST['edit-area'];
 $especialidad = $_POST['edit-especialidad'];
 $provincia = $_POST['edit-provincia'];
@@ -30,7 +30,7 @@ $resultado = mysqli_fetch_array($consulta); // guarda en una variable el resulta
 
 if($resultado['filas']>0){  // Si hay un resultado en el array...
 
-    $action = "UPDATE users SET user='$usuario',password='$password',nombre='$nombre',apellido='$apellido',rol='$cargo',dob='$dob',puntaje='$puntaje',documentacion='$documentacion',provincia='$provincia',region='$region',distrito='$distrito',area='$area',especialidad='$especialidad' WHERE user = '$globaluser'" ; // 
+    $action = "UPDATE users SET password='$password',nombre='$nombre',apellido='$apellido',dob='$dob',puntaje='$puntaje',documentacion='$documentacion',provincia='$provincia',region='$region',distrito='$distrito',area='$area',especialidad='$especialidad' WHERE user = '$globaluser'" ; // 
     $consultaedit = mysqli_query($conexion,$action); // une los datos de conexion + la consulta SQL
    
 
