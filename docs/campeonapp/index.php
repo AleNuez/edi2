f1be3c9916bff6f9d7e0b523c6bf8fedfbb5d649
alejandro.nunez <alejandro.nunez@wundermanthompson.com>
2020-08-12 22:49:56 -0300

    primeros cambios en nuevo proyecto

---
+++ docs/campeonapp/index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CampeonAPP</title>
+</head>
+<body>
+<div class="main-contenedor">
+<h1>Bienvenido</h1>
+    <div class="main-login">
+        <div class="main-login-campos">
+        <p>Ingresá</p>
+        <label for="main-user-field">Usuario</label>
+        <input class="main-user-field" name="main-user-field" type="text">
+        <label for="main-password-field">Clave</label>
+        <input class="main-password-field" name="main-password-field" type="text">
+        <input type="submit" name="main-entrar" id="main-entrar">
+        </div>
+    </div>
+</div>
+</body>
+</html>
